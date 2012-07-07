adce14740d78b9962ea203c8c676b12447f4338b
Joe Esposito <joe@joeyespo.com>
2012-07-07 18:46:28 -0400

    Project layout.

---
+++ README.md
@@ -0,0 +1,5 @@
+Hypermock
+=========
+
+Hypermock is a browser tool to add a thin, interactive workflow with inline
+comments to a collection of mockups suitable for demo or developer handoff.
---
+++ hypermock/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""\
+Hypermock
+
+Organize your mockups for implementation or for demo.
+"""
+
+__version__ = '0.1'
+__description__ = '\n\n'.join(__doc__.split('\n\n')[1:])
