0e6a19ff1837a07f9e8171ded63a5df68dec9c51
Joe Esposito <joe@joeyespo.com>
2012-07-08 00:42:13 -0400

    Disabled caching for mockup images.

--- hypermock/config.py
+++ hypermock/config.py
@@ -9,3 +9,5 @@ Override them using command-line arguments instead.
 HOST = 'localhost'
 PORT = 8080
 PROJECT_PATH = '.'
+
+WORKSPACE_SEND_FILE_OPTIONS = { 'cache_timeout': None, 'add_etags': False }
