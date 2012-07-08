6f43751dcbf79778d1e7431e95f39031132d3902
Joe Esposito <joe@joeyespo.com>
2012-07-08 14:48:42 -0400

    Handled dependent configuration values.

--- hypermock/command.py
+++ hypermock/command.py
@@ -9,13 +9,15 @@ def main():
     # Get contextual information
     project_directory = os.path.abspath(os.path.normpath('.'))
     relationships_filename = 'relationships.json'
-    relationships_path = os.path.join(project_directory, relationships_filename)
     
     # Set default config values
     app.config.setdefault('PROJECT_DIRECTORY', project_directory)
     app.config.setdefault('WORKSPACE_SEND_FILE_OPTIONS', {})
     app.config.setdefault('SUPPORTED_IMAGE_EXTENSIONS', [])
     app.config.setdefault('RELATIONSHIPS_FILENAME', relationships_filename)
+    
+    # Set dependent default config values
+    relationships_path = os.path.join(app.config['PROJECT_DIRECTORY'], app.config['RELATIONSHIPS_FILENAME'])
     app.config.setdefault('RELATIONSHIPS_PATH', relationships_path)
     
     # TODO: command-line processing
