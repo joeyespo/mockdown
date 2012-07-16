7a907269c852867212ec50d16b688aa7c21fff0b
Joe Esposito <joe@joeyespo.com>
2012-07-16 08:16:41 -0400

    Added optional base_path parameter to list_files.

--- hypermock/util.py
+++ hypermock/util.py
@@ -7,14 +7,14 @@ except ImportError:
     import json
 
 
-def list_files(directory, extensions=None):
+def list_files(directory, extensions=None, base_path=None):
     """Get a list of files, recursively, for the specified directory and list of extensions."""
     files = []
     for dirname, dirnames, filenames in os.walk(directory):
         for filename in filenames:
             if not extensions or os.path.splitext(filename)[1] in extensions:
                 files.append(os.path.relpath(os.path.join(dirname, filename), directory))
-    return files
+    return map(lambda filename: os.path.join(base_path, filename), files) if base_path else files
 
 
 def read_json(path):
