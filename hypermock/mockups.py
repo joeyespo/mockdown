db3bfc173d8ead66c7783af9cc7072753b17c6b0
Joe Esposito <joe@joeyespo.com>
2012-07-19 08:06:56 -0400

    Merged util with mockups.

--- hypermock/mockups.py
+++ hypermock/mockups.py
@@ -1,7 +1,10 @@
 import os
 import posixpath
 from flask import url_for
-from .util import list_files, read_json
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 
 class MockupGraph(object):
@@ -93,3 +96,22 @@ def mockup_from_path(relationships, path):
         if mockup.path == path:
             return mockup
     return None
+
+
+def list_files(directory, extensions=None, base_path=None):
+    """Get a list of files, recursively, for the specified directory and list of extensions."""
+    files = []
+    for dirname, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if not extensions or os.path.splitext(filename)[1] in extensions:
+                files.append(os.path.relpath(os.path.join(dirname, filename), directory))
+    return map(lambda filename: os.path.join(base_path, filename), files) if base_path else files
+
+
+def read_json(path):
+    """Returns the contents of the specified file, if it exists; otherwise, None."""
+    if not os.path.exists(path):
+        return None
+    
+    with open(path) as f:
+        return json.load(f)
