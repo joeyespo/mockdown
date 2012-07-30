d9dc4fe41a2751f6c9b716971e666412a6d518bb
Joe Esposito <joe@joeyespo.com>
2012-07-29 21:10:54 -0400

    Use spaces.

--- hypermock/static/hypermock.js
+++ hypermock/static/hypermock.js
@@ -1,19 +1,19 @@
 // hypermock.js
 
 $('.pointerTool').click(function() {
-	// TODO: implement
-	alert('TODO: Pointer');
-	return false;
+    // TODO: implement
+    alert('TODO: Pointer');
+    return false;
 });
 
 $('.commentTool').click(function() {
-	// TODO: implement
-	alert('TODO: Add comment');
-	return false;
+    // TODO: implement
+    alert('TODO: Add comment');
+    return false;
 });
 
 $('.hyperlinkTool').click(function() {
-	// TODO: implement
-	alert('TODO: Add link');
-	return false;
+    // TODO: implement
+    alert('TODO: Add link');
+    return false;
 });
