091a046e6b7a48db561a0a69f90c298e5062408b
Joe Esposito <joe@joeyespo.com>
2012-07-29 20:46:18 -0400

    Added initial code for the tools.

--- hypermock/static/hypermock.js
+++ hypermock/static/hypermock.js
@@ -1,1 +1,13 @@
 // hypermock.js
+
+$('.addComment').click(function() {
+	// TODO: implement
+	alert('TODO: Add comment');
+	return false;
+});
+
+$('.addLink').click(function() {
+	// TODO: implement
+	alert('TODO: Add link');
+	return false;
+});
