b2f34c477d63f812183d43a5b302a565ba03493d
Joe Esposito <joe@joeyespo.com>
2012-08-05 17:24:56 -0400

    Switched to the editor class.

--- hypermock/static/hypermock.js
+++ hypermock/static/hypermock.js
@@ -1,127 +1,59 @@
 // hypermock.js
 
-// TODO: Clean this code up
 // TODO: better drag-to-create functionality
 
-
-// Tool info
-var pointerTool = 'pointer';
-var availableTools = [pointerTool, 'comment', 'hyperlink'];
-var selectedTool = '';
-
-
-// Behavior
-var currentTags = [];
-var currentUiTags = [];
-
-function setTool(tool) {
+function updateTools() {
     $('.tool').removeClass('selected');
-    $('.tool[data-tool="' + tool + '"]').addClass('selected');
-    $('.preview').toggleClass('toolSelected', tool !== pointerTool);
-    selectedTool = tool;
+    $('.tool[data-tool="' + editor.selectedTool.name + '"]').addClass('selected');
+    $('.preview').toggleClass('toolSelected', editor.selectedTool !== Editor.PointerTool);
 }
 
-function createBox(rect) {
-    switch(selectedTool) {
-        case 'comment': return createComment(rect);
-        case 'hyperlink': return createHyperlink(rect);
-    }
-}
-
-function createComment(rect) {
-    addTag('comment', rect);
-    // TODO: begin editing comment
-}
-
-function createHyperlink(rect) {
-    addTag('hyperlink', rect);
-    // TODO: show potential mockups to link to
+function updatePreview() {
+    var bounds = editor.newTagPreview;
+    var properties = bounds !== null ?
+        {display: 'block', left: bounds.x, top: bounds.y, width: bounds.width, height: bounds.height} :
+        {display: 'none'};
+    $('.overlayPreview').css(properties);
 }
 
 
-// Drag functionality
-var drag = null;
+$('.tool').click(function() {
+    editor.selectTool($(this).data('tool'));
+    updateTools();
+    return false;
+});
+
 $('.preview').mousedown(function(e) {
-    if(!$(this).hasClass('toolSelected'))
-        return false;
-    drag = {x: e.clientX, y: e.clientY};
-    $('.overlayPreview').css({left: drag.x, top: drag.y, width: 0, height: 0}).show();
+    if(editor.selectedTool !== Editor.PointerTool) {
+        editor.startNewTag(e.clientX, e.clientY);
+        updatePreview();
+    }
     return false;
 });
 $('.preview, .overlayPreview').mousemove(function(e) {
-    if(drag === null)
+    if(editor.newTagPreview === null)
         return true;
-    var popup = $('.overlayPreview');
-    drag = {x: e.clientX, y: e.clientY};
-    popup.css({width: Math.max(drag.x - popup.position().left, 0), height: Math.max(drag.y - popup.position().top, 0)});
+    editor.newTagPreview.update(e.clientX, e.clientY);
+    updatePreview();
     return false;
 });
 $('.preview, .overlayPreview').mouseup(function(e) {
-    if(drag === null)
+    if(editor.newTagPreview === null)
         return true;
-    var popup = $('.overlayPreview');
-    var rect = {x: popup.position().left, y: popup.position().top, width: popup.width(), height: popup.height()};
-    drag = null;
-    popup.css({left: 0, top: 0, width: 0, height: 0}).hide();
-    if(rect.width > 0 && rect.height > 0)
-        createBox(rect);
+    var tag = editor.endNewTag();
+    updatePreview();
+    alert('TODO: Create ' + tag.type + ' tag');
+    // TODO: begin editing tag
     return false;
 });
 
-function addTag(type, bounds) {
-    var tag = {type: type, bounds: bounds};
-    currentTags.push(tag);
-    createUiTag(tag);
-    return tag;
-}
-
-function removeTag(tag) {
-    // TODO: implement
-}
-
-function createUiTag(tag) {
-    // TODO: create element
-    alert('TODO: Create ' + tag.type + ' tag');
-    var uiTag = {
-        element: null,
-        tag: tag
-    };
-    currentUiTags.push(uiTag);
-    return uiTag;
-}
-
-function deleteUiTag(uiTag) {
-    // TODO: implement
-}
-
 
 // Initialization
-function initTools() {
-    $.each(availableTools, function(index, tool) {
-        initTool(tool);
-    });
-}
-
-function initTool(tool) {
-    var element = $('.tool[data-tool="' + tool + '"]');
-    if(element.length === 0) {
-        alert('"' + tool + '" tool not found.');
-        return true;
-    }
-    element.click(function() {
-        setTool(tool);
-        return false;
-    });
-}
-
 function initTags() {
-    $.each(sessionTags || [], function(index, tag) {
-        addTag(tag.type, tag.bounds);
+    $.each(window.sessionTags || [], function(index, tag) {
+        editor.tags.create(tag.type, tag.bounds);
     });
 }
-
-$(function() {
-    initTools();
-    setTool(pointerTool);
-    initTags();
-});
+var editor = new Editor();
+updateTools();
+$(initTags);
