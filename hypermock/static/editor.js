99af90a41e9555488e4bfcca81ebaa62719b3bd0
Joe Esposito <joe@joeyespo.com>
2012-08-05 17:20:42 -0400

    Relabeled some classes.

--- hypermock/static/editor.js
+++ hypermock/static/editor.js
@@ -1,12 +1,14 @@
 // editor.js
 
 
-// Data models
+// Tool model
 function Tool(name, tagClass) {
     this.name = name;
     this.tag = tagClass;
 }
 
+
+// Tag model
 function Tag(type, bounds) {
     this.type = type;
     this.bounds = bounds || {x: 0, y: 0, width: 0, height: 0};
@@ -22,14 +24,14 @@ Tag.Create = function(type, bounds, data) {
             return null;
     }
 };
-
+// Comment tag
 function CommentTag(bounds, text) {
     Tag.call(this, CommentTag.Name, bounds, text);
     this.text = text || '';
 }
 CommentTag.Name = 'comment';
 CommentTag.prototype = new Tag();
-
+// Hyperlink tag
 function HyperlinkTag(bounds, href) {
     Tag.call(this, HyperlinkTag.Name, bounds, href);
     this.href = href || '';
