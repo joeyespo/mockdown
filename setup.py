7f2a11545f86a723f5d84f39b19336f06f123055
Joe Esposito <joe@joeyespo.com>
2012-08-25 11:32:36 -0400

    PEP8 the setup.

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ def read(fname):
 
 
 setup(
-    name = package.__name__,
-    author = 'Joe Esposito',
-    author_email = 'joe@joeyespo.com',
-    url = 'mockdownapp.com',
-    license = 'MIT',
-    version = package.__version__,
-    description = package.__description__,
-    long_description = read('README.md'),
-    packages = find_packages(),
-    entry_points = {'console_scripts': ['mockdown = mockdown.command:main']},
-    install_requires = read('requirements.txt').split('\n'),
+    name=package.__name__,
+    author='Joe Esposito',
+    author_email='joe@joeyespo.com',
+    url='mockdownapp.com',
+    license='MIT',
+    version=package.__version__,
+    description=package.__description__,
+    long_description=read('README.md'),
+    packages=find_packages(),
+    entry_points={'console_scripts': ['mockdown = mockdown.command:main']},
+    install_requires=read('requirements.txt').split('\n'),
 )
